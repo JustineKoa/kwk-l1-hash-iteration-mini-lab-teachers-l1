dbccbee6c518ac1630ecca7d62b8e1c0ae7f4cdb
Justine Koa <justinekoa@gmail.com>
2018-06-20 20:50:50 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -16,7 +16,7 @@ def iterate_through_hash
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
   olympics_hash =  {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012", :Atlanta => "1996"}
   olympics_hash.each do |location, date|
-    puts "The #{location} summer olympics took place in #{date}"
+    puts "The #{location} summer olympics took place in #{date}."
   end
 end
 
